a98da8b794f27bdb270c2d7008a69db0f1abedaf
GintsEngelen <GintsEngelen@gmail.com>
2020-02-21 07:47:41 +0100

    merging

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from Library import Library
 from Book import Book
-import numpy as np
 
 with open("a_example.txt", 'r', encoding='utf-8') as infile, open("a_example_output.txt", 'w', encoding='utf-8') as outfile:
     counter = 0;
@@ -10,22 +9,21 @@ with open("a_example.txt", 'r', encoding='utf-8') as infile, open("a_example_out
     librariesAmount = line1[1]
     daysForScanning = line1[2]
 
+    librariesBooksAmount = []
+    librariesBooks = []
+    librariesSignUpTime = []
+    librariesShippingCapacity = []
     bookScores = [int(number) for number in infile.readline().split()]
 
-    libraries = []
     libraryInfo = [int(number) for number in infile.readline().split()]
 
     while libraryInfo:
+        librariesBooksAmount.append(libraryInfo[0])
+        librariesSignUpTime.append(libraryInfo[1])
+        librariesShippingCapacity.append(libraryInfo[2])
+
         booksInLibrary = [int(number) for number in infile.readline().split()]
-        bookIdsSet = set(booksInLibrary)
-        bookSet = []
-
-        for book_id in bookIdsSet:
-            bookSet.append(Book(book_id, bookScores[book_id]))
-
-        libraries.append(Library(libraryInfo[1], libraryInfo[2], bookSet))
-
-        libraryInfo = [int(number) for number in infile.readline().split()]
+        librariesBooks.append(set(booksInLibrary))
 
     libraryScores = []
     for i in range(librariesAmount):
