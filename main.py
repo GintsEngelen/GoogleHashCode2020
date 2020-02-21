486cbac267b22b997ac9ae70d3cef1f42a57a4e4
GintsEngelen <gintsengelen@gmail.com>
2020-02-21 07:44:35 +0100

    trying bool numpy array

--- main.py
+++ main.py
@@ -1,35 +1,22 @@
-from Library import Library
-from Book import Book
 import numpy as np
 
 with open("a_example.txt", 'r', encoding='utf-8') as infile, open("a_example_output.txt", 'w', encoding='utf-8') as outfile:
     counter = 0;
 
-    line1 = [int(number) for number in infile.readline().split()]
-    booksAmount = line1[0]
-    librariesAmount = line1[1]
-    daysForScanning = line1[2]
+    [booksAmount, librariesAmount, daysForScanning] = [int(number) for number in infile.readline().split()]
+    bookScores = np.array([infile.readline().split()])
 
-    bookScores = [int(number) for number in infile.readline().split()]
+    libraryInfo = np.zeros((librariesAmount, 3))
+    libraryBooks = np.zeros((librariesAmount, booksAmount), dtype=bool)
 
-    libraries = []
-    libraryInfo = [int(number) for number in infile.readline().split()]
-
-    while libraryInfo:
-        booksInLibrary = [int(number) for number in infile.readline().split()]
-        bookIdsSet = set(booksInLibrary)
-        bookSet = []
-
-        for book_id in bookIdsSet:
-            bookSet.append(Book(book_id, bookScores[book_id]))
-
-        libraries.append(Library(libraryInfo[1], libraryInfo[2], bookSet))
-
-        libraryInfo = [int(number) for number in infile.readline().split()]
-
-    libraryScores = []
     for i in range(librariesAmount):
-        libraryScores.append(libraries[i].scoreTotal(daysForScanning))
 
-    print(libraryScores)
+        libraryInfo[i] = np.array([infile.readline().split()], dtype=int)
+        libraryBooksIndexes = np.array([infile.readline().split()], dtype=int)
 
+        libraryBooks[i, libraryBooksIndexes] = True
+
+    print(libraryInfo)
+    print(libraryBooks)
+
+    print()
